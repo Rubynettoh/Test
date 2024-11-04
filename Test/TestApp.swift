22a8814279983da2fe06511fe88e7cbc1a62556e
Cristian Scarano <cristianscarano20@gmail.com>
2024-11-04 15:18:54 +0100

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Cristian Scarano on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
